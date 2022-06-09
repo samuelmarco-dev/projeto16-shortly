eefe28efe5d22ba07c661fcd21a780c4b15860db
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-09 00:09:26 -0300

    feat: estrutura de autenticação de rota e relacionamento entre users e links

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import dotenv from 'dotenv';
 
 import db from '../database.js';
@@ -10,15 +11,39 @@ export async function createShortUrlUser(req, res) {
     console.log('Token na requisição', token);
     if(!token) return res.status(401).send('User is not authorized');
     
+    const {url} = req.body;
+
     try {
         console.log('teste');
         const secretKey = process.env.JWT_SECRET;
-        jwt.verify(token, secretKey);
-        console.log('Verificação do token', jwt.verify(token, secretKey));
+        const dados = jwt.verify(token, secretKey);
+        console.log('Verificação do token', jwt.verify(token, secretKey), dados);
 
         //buscar a sessão do usuário
-        //encontrar o usuário que fez login
+        const session = await db.query(`
+            SELECT * FROM sessions WHERE token = $1
+        `, [token]);
+        console.log('sessão encontrada', session);
 
+        //encontrar o usuário que fez login
+        const user = await db.query(`
+            SELECT * FROM users WHERE id = $1
+        `, [session.userId]);
+        console.log('usuário encontrado', user);
+
+        //criar url encurtada
+        const urlShort = nanoid();
+        
+        await db.query(`
+            INSERT INTO links (url, shortUrl)
+            VALUES ($1, $2)
+        `, [url, urlShort]);
+        await db.query(`
+            INSERT INTO "linksUsers" ("linkId", "userId", views)
+            VALUES ((SELECT id FROM links WHERE url = $1), $2, 0)
+        `, [url, user.id]);
+        
+        res.status(201).send(urlShort);
     } catch (error) {
         console.log(chalk.red(error));
         res.sendStatus(500);
