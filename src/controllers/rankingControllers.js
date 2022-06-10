ffb2283d4718a6620469799fe630f8e56dfb9812
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-09 23:45:20 -0300

    feat: busca de ranking dos usuários; Restando implementação de middlewares

--- src/controllers/rankingControllers.js
+++ src/controllers/rankingControllers.js
@@ -84,7 +84,21 @@ export async function detailsUrlsUser(req, res){
 
 export async function getRanking(req, res){
     try {
-        
+        const result = await db.query(`
+            SELECT users.id, users.name, 
+            COUNT("linksUsers"."linkId") as "linksCount", 
+            SUM("linksUsers"."views") as "visitCount"
+            FROM "linksUsers"
+            LEFT JOIN users ON "linksUsers"."userId" = users.id
+            GROUP BY "linksUsers"."userId", users.id 
+            ORDER BY "visitCount" DESC LIMIT 10
+        `, []);
+        console.log('resultado da query', result);
+
+        const verify = result.rowCount === 0 || result.rows.length === 0;
+        if(verify) return res.sendStatus(404);
+
+        res.send(result.rows).status(200);
     } catch (error) {
         console.log(chalk.red(error));
         res.sendStatus(500);
