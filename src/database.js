6038f5c1325a521848b39938165cb4d1499bc408
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-10 15:54:22 -0300

    update config deploy

--- src/database.js
+++ src/database.js
@@ -4,8 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pg;
 
-const db = new Pool({
+const database = {
     connectionString: process.env.DATABASE_URL
-});
+};
+
+if(process.env.MODE === "PROD"){
+    database.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(database);
 
 export default db;
